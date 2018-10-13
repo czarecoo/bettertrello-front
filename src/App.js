0127da3a18604cebc098056691df163179a4e767
czarecoo@gmail.com <czarecoo@gmail.com>
2018-10-13 18:25:11 +0200

    Added first working example

--- src/App.js
+++ src/App.js
@@ -1,28 +1,55 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+
+	constructor(props) {
+		super(props);
+		this.state = {
+			contacts: []
+		};
+	}
+
+	componentDidMount() {
+		axios.get('http://localhost:8080/contacts')
+			.then(res => {
+				this.setState({ contacts: res.data });
+			});
+	}
+
+	render() {
+		return (
+			<div className="container">
+				<div className="panel panel-default">
+					<div className="panel-heading">
+						<h3 className="panel-title">
+							CONTACTS LIST
+            			</h3>
+					</div>
+					<div className="panel-body">
+						<h4><Link to="/create"><span className="glyphicon glyphicon-plus-sign" aria-hidden="true"></span> Add Contact</Link></h4>
+						<table className="table table-stripe">
+							<thead>
+								<tr>
+									<th>Name</th>
+									<th>Address</th>
+								</tr>
+							</thead>
+							<tbody>
+								{this.state.contacts.map((c, index) =>
+									<tr key={index}>
+										<td><Link to={`/show/${c.id}`}>{c.name}</Link></td>
+										<td>{c.address}</td>
+									</tr>
+								)}
+							</tbody>
+						</table>
+					</div>
+				</div>
+			</div>
+		);
+	}
 }
 
 export default App;
