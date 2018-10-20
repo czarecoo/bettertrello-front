5bee1f336849b35ba6b3fa661cb83d07c0bf3693
czarecoo@gmail.com <czarecoo@gmail.com>
2018-10-20 18:36:12 +0200

    Added header

--- src/index.js
+++ src/index.js
@@ -8,10 +8,28 @@ import './App.css';
 import Edit from './components/Edit';
 import Create from './components/Create';
 import Board from './components/Board';
+import { Link } from 'react-router-dom';
 
 ReactDOM.render(
 	<Router>
 		<div>
+			<div className="header">
+				<div class="block left">
+					<Link className="btn btn-info btn-sm homebtn" to={`/`}>
+						<img src="home.png" alt="home" height="25" width="25"></img>
+					</Link>
+				</div>
+				<div class="block center">
+					<Link to={`/`}>
+						<img className="logo" src="logo.png" alt="logo" height="30" width="170"></img>
+					</Link>
+				</div>
+				<div class="block right">
+					<Link className="btn btn-info btn-sm homebtn" to={`/`}>
+						<img src="plus.png" alt="plus" height="25" width="25"></img>
+					</Link>
+				</div>
+			</div>
 			<Route exact path='/' component={App} />
 			<Route path='/edit/:id' component={Edit} />
 			<Route path='/create' component={Create} />
