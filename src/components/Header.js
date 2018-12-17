ed0845aecd76b85ca17ab9a61e026acd35135f26
czarecoo@gmail.com <czarecoo@gmail.com>
2018-12-17 19:04:20 +0100

    Added user popover

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,9 +3,11 @@ import { Link } from 'react-router-dom';
 import home from './home.png';
 import logo from './logo.png';
 import plus from './plus.png';
+import user from './user.png';
 import logout from './logout.png';
 import notifications from './notifications.png';
 import Notifications from './Notifications';
+import Create from './Create';
 import { Popover, PopoverHeader, PopoverBody } from 'reactstrap';
 
 class Header extends React.Component {
@@ -13,14 +15,16 @@ class Header extends React.Component {
 		super(props);
 		this.toggle = this.toggle.bind(this);
 		this.state = {
-			popoverOpen: false, notifications: []
+			popoverOpen: "", notifications: []
 		};
 	}
-
-	toggle() {
-		this.setState({
-			popoverOpen: !this.state.popoverOpen
-		});
+	toggle(event) {
+		if (this.state.popoverOpen === event.target.id || this.state.popoverOpen !== "") {
+			this.setState({ popoverOpen: "" });
+		}
+		else {
+			this.setState({ popoverOpen: event.target.id });
+		}
 	}
 	render() {
 		if (this.props.isLoggedIn) {
@@ -37,19 +41,36 @@ class Header extends React.Component {
 						</Link>
 					</div>
 					<div className="block right">
-						<Link className="btn btn-info btn-sm homebtn" to={`/create`}>
+						<button id="PopoverCreate" onClick={this.toggle} className="btn btn-info btn-sm homebtn" to={`/create`}>
 							<img src={plus} alt="plus" height="25" width="25"></img>
-						</Link>
-						<button id="Popover1" onClick={this.toggle} className="btn btn-info btn-sm homebtn">
+						</button>
+						<Popover placement="bottom" isOpen={this.state.popoverOpen === "PopoverCreate"} target="PopoverCreate" toggle={this.toggle}>
+							<PopoverHeader>Create board</PopoverHeader>
+							<PopoverBody>
+								<Create />
+							</PopoverBody>
+						</Popover>
+						<button id="PopoverNotifications" onClick={this.toggle} className="btn btn-info btn-sm homebtn">
 							<img src={notifications} alt="notifications" height="25" width="25"></img>
 						</button>
-						<Popover placement="bottom" isOpen={this.state.popoverOpen} target="Popover1" toggle={this.toggle}>
+						<Popover placement="bottom" isOpen={this.state.popoverOpen === "PopoverNotifications"} target="PopoverNotifications" toggle={this.toggle}>
 							<PopoverHeader>Notifications</PopoverHeader>
 							<PopoverBody>
-								<Notifications />
+								<Notifications notifications={this.state.notifications} />
 							</PopoverBody>
 						</Popover>
 
+						<button id="PopoverUser" onClick={this.toggle} className="btn btn-info btn-sm homebtn">
+							<img src={user} alt="user" height="25" width="25"></img>
+						</button>
+						<Popover placement="bottom" isOpen={this.state.popoverOpen === "PopoverUser"} target="PopoverUser" toggle={this.toggle}>
+							<PopoverHeader>User panel</PopoverHeader>
+							<PopoverBody>
+								<Link onClick={() => this.props.logOut()} to={`/`} >
+									Logout
+								</Link>
+							</PopoverBody>
+						</Popover>
 						<Link onClick={() => this.props.logOut()} className="btn btn-info btn-sm homebtn" to={`/`} >
 							<img src={logout} alt="logout" height="25" width="25"></img>
 						</Link>
